69f6833a0f9eb1776f0ef2eaa68c2e9c5b076c46
geanusksama <engcom-ia@hotmail.com>
2022-10-27 13:47:28 +0000

    Ajustando o código

--- cidades/settings.py
+++ cidades/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8u2qv!7*%q%z0$99!uaiq7@m92dhmkz^@gc@hnf*%1mao$s*cm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -103,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
--- home/models.py
+++ home/models.py
@@ -1,3 +1,23 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+class Detalhes(models.Model):
+    detalhe = nome = models.ForeignKey(Cidade, on_delete=models.DO_NOTHING)
+    nome = models.CharField(max_length=50)
+    nullable = models.BooleanField(default=True)
+    dataFundacao = models.DateTimeField(auto_add=True)
+    populacaoEstimada = models.IntegerField()
+    altitude = models.IntegerField()
+    idh = models.DecimalField(decimal_places=1, max_digits=10)
+    site = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
